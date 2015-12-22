62bed05319ce2a499e1a93c8db942bd6260a05cb
Michał <micles92@gmail.com>
2015-12-22 20:26:12 +0100

    Dodawanie produktow do bazy danych SQL

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,12 @@ public class Main {
         GenericShop obj = new GenericShop(new DataBaseProcessor());
 
 
-        obj.addProduct(new Computer("Asus", 999, 16, 2.7));
-        obj.addProduct(new Computer("Asus1", 929, 22, 2.7));
-
-        obj.addProduct(new Phone("Nokia", 66, 4.5, 5.6));
-        obj.addProduct(new Phone("Nokia22", 26, 4.3, 3.6));
+    //        obj.addProduct(new Computer("Asus", 999, 16, 2.7));
+    //        obj.addProduct(new Computer("Asus1", 929, 22, 2.7));
+    //
+    //        obj.addProduct(new Phone("Nokia", 66, 4.5, 5.6));
+    //        obj.addProduct(new Phone("Nokia22", 26, 4.3, 3.6));
+        obj.addProduct(new Phone("LUMIA", 6, 6.0, 6.0));
 
 //        obj.displayDiscount();
 //        //obj.addProduct(new Product("nazwa1", 5.5));
